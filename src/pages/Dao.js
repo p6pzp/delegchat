dc12e132a34812049b40cc25366a3c3d36fc3d8e
Juan M <jm@guide42.com>
2023-07-22 21:46:25 +0200

    Add mint DelegNoun button with Sismo

--- src/pages/Dao.js
+++ src/pages/Dao.js
@@ -1,12 +1,51 @@
+import { useState } from 'react'
 import { useRouteLoaderData } from 'react-router-dom'
+import { useAccount } from 'wagmi'
+import { encodeAbiParameters } from 'viem'
+import { SismoConnectButton } from '@sismo-core/sismo-connect-react'
 import Authenticated from '../layout/Authenticated'
+
+const signMessage = (address) => encodeAbiParameters(
+  [{ type: 'address', name: 'delegateAddress' }],
+  [address]
+)
 
 function Dao() {
   const { dao } = useRouteLoaderData('dao')
+  const { address } = useAccount()
+
+  const [responseBytes, setResponseBytes] = useState(null)
 
   return (
     <Authenticated>
       <h2>{dao.name}</h2>
+      {responseBytes && (
+        <span>Yay!</span>
+      )}
+      {!responseBytes && (
+        <SismoConnectButton
+          config={{
+            appId: process.env.REACT_APP_SISMO_APP_ID,
+            vault: {
+              impersonate: [
+                address,
+              ],
+            },
+          }}
+          auths={[
+            {
+              authType: 0, // AuthType.VAULT
+            },
+            {
+              authType: 3, // AuthType.EVM_ACCOUNT
+            },
+          ]}
+          claims={dao.claims}
+          signature={{ message: signMessage(address) }}
+          onResponseBytes={(responseBytes) => setResponseBytes(responseBytes)}
+          text="Mint a DelegNoun"
+        />
+      )}
     </Authenticated>
   )
 }
