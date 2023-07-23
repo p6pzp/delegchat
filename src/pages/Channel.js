1ae3f9dc5cd59dcd453429e80d8bc848c2cf373a
Juan M <jm@guide42.com>
2023-07-23 03:07:54 +0200

    Move authenticated to channel

--- src/pages/Channel.js
+++ src/pages/Channel.js
@@ -1,10 +1,39 @@
-import Authenticated from '../layout/Authenticated'
+import { useRouteLoaderData } from 'react-router-dom'
+import { useAccount, useNetwork } from 'wagmi'
+import DaoInfo from '../components/DaoInfo'
+import WalletConnect from '../components/WalletConnect'
+import ErrorMessage from '../components/ErrorMessage'
 
 function Channel() {
+  const { dao } = useRouteLoaderData('dao')
+  const { address } = useAccount()
+  const { chain } = useNetwork()
+
+  if (!chain || !address) {
+    return (
+      <>
+        <DaoInfo dao={dao} />
+        <WalletConnect />
+      </>
+    )
+  }
+
+  if (!(chain.id in dao.contracts)) {
+    return (
+      <>
+        <DaoInfo dao={dao} />
+        <ErrorMessage
+          error={{ message: `Chain ${chain.name} not supported` }}
+        />
+      </>
+    )
+  }
+
   return (
-    <Authenticated>
+    <>
+      <DaoInfo dao={dao} />
       <span>Channel</span>
-    </Authenticated>
+    </>
   )
 }
 
